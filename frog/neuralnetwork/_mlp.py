3e50aef128130672552ec3322c3e99b6bf79d803
ppiper <ppiper@pop-os.localdomain>
2023-11-08 13:22:22 -0300

    add new normalization methods

--- frog/neuralnetwork/_mlp.py
+++ frog/neuralnetwork/_mlp.py
@@ -1,23 +1,31 @@
 
 import tensorflow as tf
+from typing import Union
 
-def get_model ( num_inputs , num_outputs , num_layers , num_neurons ):
+def get_model ( 
+    num_inputs : int, 
+    num_outputs : int, 
+    num_layers : int, 
+    num_neurons : int,
+    activation : str = 'tanh',
+    optimizer : Union[callable, str] = tf.keras.optimizers.Adam(),
+    loss : dict = { 'output_value': 'mean_squared_error'}):
 
     # Input layer
     ph_input = tf.keras.Input( shape =( num_inputs ,) ,name='input_placeholder')
     # Hidden layers
-    hidden_layer = tf.keras.layers.Dense ( num_neurons , activation ='tanh')( ph_input )
+    hidden_layer = tf.keras.layers.Dense ( num_neurons , activation = activation)( ph_input )
     for layer in range ( num_layers ):
-        hidden_layer = tf.keras.layers.Dense ( num_neurons , activation ='tanh')( hidden_layer )
+        hidden_layer = tf.keras.layers.Dense ( num_neurons , activation = activation)( hidden_layer )
 
 
     # Output layer
     output = tf.keras.layers.Dense ( num_outputs , activation ='linear',name='output_value')( hidden_layer)
     model = tf.keras.Model ( inputs =[ ph_input ], outputs =[ output ])
     # Optimizer
-    my_adam = tf.keras.optimizers.Adam()
+    #my_adam = tf.keras.optimizers.Adam()
     # Compilation
-    model.compile ( optimizer =my_adam , loss ={ 'output_value': 'mean_squared_error'})
+    model.compile ( optimizer = optimizer , loss = loss)
     return model
 
 def mlp(inputs_train, outputs_train, inputs_validation, outputs_validation, layers, fit_kwargs):
--- frog/normalization/_normalization.py
+++ frog/normalization/_normalization.py
@@ -1,7 +1,113 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+
+class DataHandlerMinMaxScaler(MinMaxScaler):
+    def __init__(self, datahandler, feature_range=(0,1)):
+        self.datahandler = datahandler
+        self.scalers = {}
+        self.feature_range = feature_range
+
+        for dataset in self.datahandler.datasets:
+            self.scalers[dataset] = MinMaxScaler(feature_range=self.feature_range)
+
+    def fit(self, X):
+        print('fit')
+        for dataset in self.datahandler.datasets:
+            self.scalers[dataset].fit(self.datahandler(X)[dataset])
+
+    def transform(self, X):
+        print('transform')
+        Xt = X.copy()
+        for dataset in self.datahandler.datasets:
+            Xt[self.datahandler.get_index(dataset)] = self.scalers[dataset].transform(X[self.datahandler.get_index(dataset)])
+        return Xt
+    
+    def inverse_transform(self, X):
+        print('inverse_transform')
+        Xt = X.copy()
+        for dataset in self.datahandler.datasets:
+            Xt[self.datahandler.get_index(dataset)] = self.scalers[dataset].inverse_transform(X[self.datahandler.get_index(dataset)])
+        return Xt
+    
+    def fit_transform(self, X, y=None):
+        print('fit_transform')
+        self.fit(X)
+        return self.transform(X)
+
+class DataHandlerStandardNormalization(BaseEstimator, TransformerMixin):
+    def __init__(self,datahandler, bounds):
+        self.bounds = bounds
+        self.datahandler = datahandler
+
+    def fit(self, X, y=None):
+        print('fit')
+        self.mean = {}
+        self.min = {}
+        self.max = {}
+        self.std = {}
+        for dataset in self.datahandler.datasets:
+            self.mean[dataset] = np.mean(self.datahandler(X)[dataset], axis=0)
+            self.std[dataset] = np.std(self.datahandler(X)[dataset], axis=0)
+            self.min[dataset] = np.min(self.datahandler(X)[dataset], axis=0)
+            self.max[dataset] = np.max(self.datahandler(X)[dataset], axis=0)
+    
+    def transform(self, X, y=None):
+        print('transform')
+        for dataset in self.datahandler.datasets:
+            X[self.datahandler.get_index(dataset)] -=  self.mean[dataset]
+            X[self.datahandler.get_index(dataset)] = (self.bounds[1]-self.bounds[0]) * (X[self.datahandler.get_index(dataset)]) / (self.std[dataset]) + self.bounds[0]
+        return X
+
+    def inverse_transform(self, X, y=None):
+        print('inverse_transform')
+        for dataset in self.datahandler.datasets:
+            X[self.datahandler.get_index(dataset)] = (self.std[dataset]) * (X[self.datahandler.get_index(dataset)] - self.bounds[0]) / (self.bounds[1] - self.bounds[0])
+            X[self.datahandler.get_index(dataset)] +=  self.mean[dataset]
+        return X
+
+    def fit_transform(self, X, y=None):
+        print('fit_transform')
+
+        self.fit(X)
+        return self.transform(X)
 
 class DataHandlerNormalization(BaseEstimator, TransformerMixin):
+    def __init__(self,datahandler, bounds):
+        self.bounds = bounds
+        self.datahandler = datahandler
+
+    def fit(self, X, y=None):
+        print('fit')
+        self.mean = {}
+        self.min = {}
+        self.max = {}
+        for dataset in self.datahandler.datasets:
+            self.mean[dataset] = np.mean(self.datahandler(X)[dataset], axis=0)
+            self.min[dataset] = np.min(self.datahandler(X)[dataset])
+            self.max[dataset] = np.max(self.datahandler(X)[dataset])
+    
+    def transform(self, X, y=None):
+        print('transform')
+        for dataset in self.datahandler.datasets:
+            X[self.datahandler.get_index(dataset)] -=  self.mean[dataset]
+            X[self.datahandler.get_index(dataset)] = (self.bounds[1]-self.bounds[0]) * (X[self.datahandler.get_index(dataset)] - self.min[dataset]) / (self.max[dataset] - self.min[dataset]) + self.bounds[0]
+        return X
+
+    def inverse_transform(self, X, y=None):
+        print('inverse_transform')
+        for dataset in self.datahandler.datasets:
+            X[self.datahandler.get_index(dataset)] = (self.max[dataset] - self.min[dataset]) * (X[self.datahandler.get_index(dataset)] - self.bounds[0]) / (self.bounds[1] - self.bounds[0]) + self.min[dataset]
+            X[self.datahandler.get_index(dataset)] +=  self.mean[dataset]
+        return X
+
+    def fit_transform(self, X, y=None):
+        print('fit_transform')
+
+        self.fit(X)
+        return self.transform(X)
+    
+class DataHandlerNormalization2(BaseEstimator, TransformerMixin):
     def __init__(self,datahandler, bounds):
         self.bounds = bounds
         self.datahandler = datahandler
